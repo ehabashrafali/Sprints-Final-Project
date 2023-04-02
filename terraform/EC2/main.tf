5ab494dda4209b4b3bd61630679e7b7e0a2e2a5f
ehabashrafali <116663120+ehabashrafali@users.noreply.github.com>
2023-04-02 22:33:41 +0200

    Update main.tf

--- terraform/EC2/main.tf
+++ terraform/EC2/main.tf
@@ -2,10 +2,12 @@ provider "aws" {
     region = "us-east-1"
 }
 
-resource "aws_instance" "ec2_instance" {
-    ami = "${var.ami_id}"
-    count = "${var.number_of_instances}"
-    subnet_id = "${var.subnet_id}"
-    instance_type = "${var.instance_type}"
-    key_name = "${var.ami_key_pair_name}"
+resource "aws_instance" "web_instance" {
+  ami           = "ami-0557a15b87f6559cf"
+  instance_type = "t2.micro"
+  key_name      = "TF_key"
+
+  subnet_id                   = aws_subnet.public-us-east-1a.id
+  vpc_security_group_ids      = [aws_security_group.aws_sg.id]
+  associate_public_ip_address = true
 } 
